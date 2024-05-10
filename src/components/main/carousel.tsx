cfa6fff7ad6e6f087fc2c3aba86d0d818c56db29
Superstar-Lucas <lucaslee971127@gmail.com>
2024-05-10 13:47:41 +0100

    add slick to the carousel component

--- src/components/main/carousel.tsx
+++ src/components/main/carousel.tsx
@@ -1,18 +1,50 @@
 "use client";
 
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import Image from "next/image";
 import { useEffect, useState } from "react";
 const Carousel_Component = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <div className="w-full h-[90vh] min-h-[800px]" id="marketplace_carousel">
-        <Image
-          alt="slide"
-          width={2000}
-          height={2000}
-          src="/assets/images/slide/1.jpg"
-          className="object-cover w-full h-full"
-        />
+        <Slider {...settings}>
+          <Image
+            alt="slide"
+            width={2000}
+            height={2000}
+            src="/assets/images/slide/1.jpg"
+            className="object-cover w-full h-full"
+          />
+          <Image
+            alt="slide"
+            width={2000}
+            height={2000}
+            src="/assets/images/slide/2.jpg"
+            className="object-cover w-full h-full"
+          />
+          <Image
+            alt="slide"
+            width={2000}
+            height={2000}
+            src="/assets/images/slide/3.jpg"
+            className="object-cover w-full h-full"
+          />
+        </Slider>
       </div>
     </>
   );
