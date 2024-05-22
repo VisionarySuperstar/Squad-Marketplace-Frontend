44f461cc8eed95fe277610933027ec0becf43bb2
Superstar-Lucas <lucaslee971127@gmail.com>
2024-05-22 22:38:31 +0100

    fix: NFT data manipulation
    This restores the `slice` in `data-processing` to ensure that
    `getTopNfts` and `getTopGroups` only return a maximum of 3 elements.

--- src/utils/data-processing.ts
+++ src/utils/data-processing.ts
@@ -2,11 +2,12 @@ import { IGROUP, INFT } from "@/types";
 
 export function getTopNfts(nfts: INFT[], count: number = 3): INFT[] {
   return nfts
-  .filter((_nft:INFT) => _nft.ranking)
-  .sort((a, b) => Number(a.ranking) - Number(b.ranking));
+    .filter((_nft: INFT) => _nft.ranking)
+    .sort((a, b) => Number(a.ranking) - Number(b.ranking))
+    .slice(0, count);
 }
 
-export function getNewlyMinted(nfts: INFT[], count: number = nfts.length > 30 ? 30 : nfts.length): INFT[] {
+export function getNewlyMinted(nfts: INFT[], count: number = 30): INFT[] {
   return nfts
     .sort((a, b) => Number(b.created_at) - Number(a.created_at))
     .slice(0, count);
@@ -14,6 +15,7 @@ export function getNewlyMinted(nfts: INFT[], count: number = nfts.length > 30 ? 
 
 export function getTopGroups(groups: IGROUP[], count: number = 3): IGROUP[] {
   return groups
-  .filter((_group:IGROUP) => _group.ranking)
-  .sort((a, b) => Number(a.ranking) - Number(b.ranking));
+    .filter((_group: IGROUP) => _group.ranking)
+    .sort((a, b) => Number(a.ranking) - Number(b.ranking))
+    .slice(0, count);
 }
