69bdbc9970cec60cb558121dd2ea9cd3885302e9
Superstar-Lucas <lucaslee971127@gmail.com>
2024-05-22 14:51:42 -0500

    conflict fixed

--- src/hooks/views/useMyGroups.ts
+++ src/hooks/views/useMyGroups.ts
@@ -1,17 +1,14 @@
 import { useEffect, useState } from "react";
 import useAPI from "../useAPI";
 import { IGROUP } from "@/types";
-import useAuth from "@/hooks/useAuth";
-
 
 export default function useMyGroups() {
   const api = useAPI();
   const [myGroups, setGroups] = useState<IGROUP[]>([]);
-  const { user } = useAuth();
 
   useEffect(() => {
     const fetchGroups = async () => {
-      const response = await api.post("api/getGroup", {id:user?.id});
+      const response = await api.post("api/getGroup", {});
       setGroups(response.data);
     };
 
